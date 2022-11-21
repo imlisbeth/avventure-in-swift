2f75ecc4ac06763b380d9ecba21474dc1c22e048
Павел Оливейра <lupideo@gmail.com>
2022-11-21 18:53:49 -0300

    Complete enumerations page

--- Avventure-in-Swift.playground/Pages/0-14-enumerations.xcplaygroundpage/Contents.swift
+++ Avventure-in-Swift.playground/Pages/0-14-enumerations.xcplaygroundpage/Contents.swift
@@ -10,7 +10,7 @@ enum WeatherType {
     case sun
     case cloud
     case rain
-    case wind
+    case wind(speed: Int)
     case snow
 }
 
@@ -26,6 +26,8 @@ func getHaterStatus(weather: WeatherType) -> String? {
     switch weather {
         case .sun:
             return nil
+        case .wind(let speed) where speed < 10:
+            return "meh"
         case .cloud, .wind, .snow:
             return "dislike"
         case .rain:
@@ -33,5 +35,19 @@ func getHaterStatus(weather: WeatherType) -> String? {
     }
 }
 
-getHaterStatus(weather: .rain)
+getHaterStatus(weather: WeatherType.wind(speed: 5))
+
+func knockKnock(_ caller: String?) {
+    print("Who's there?")
+    
+    switch caller {
+        case .none:
+            print("* silence *")
+        case .some(let person):
+            print(person)
+    }
+}
+
+knockKnock(nil)
+knockKnock("Павел")
 //: [Next](@next)
