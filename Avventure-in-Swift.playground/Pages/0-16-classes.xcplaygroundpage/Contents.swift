f530bf4ddeff29354cae776de35d9e755543f3e5
ليزبيث هالا <llulah@live.com>
2023-01-04 14:53:22 -0300

    class inheritance

--- Avventure-in-Swift.playground/Pages/0-16-classes.xcplaygroundpage/Contents.swift
+++ Avventure-in-Swift.playground/Pages/0-16-classes.xcplaygroundpage/Contents.swift
@@ -12,4 +12,42 @@ class Person {
     }
 }
 
+class Singer {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+
+    func sing() {
+        print("La la la la")
+    }
+}
+
+class SopranoSinger: Singer {
+    override func sing() {
+        print("Lieder and songs")
+    }
+}
+
+//var pilar = Singer(name: "Pilar Lorengar", age: 67)
+var pilar = SopranoSinger(name: "Pilar Lorengar", age: 67)
+pilar.name
+pilar.age
+pilar.sing()
+
+class HeavyMetalSinger: Singer {
+    var noiseLevel: Int
+
+    init(name: String, age: Int, noiseLevel: Int) {
+        self.noiseLevel = noiseLevel
+        super.init(name: name, age: age)
+    }
+
+    override func sing() {
+        print("Grrrrr rargh rargh rarrrrgh!")
+    }
+}
 //: [Next](@next)
